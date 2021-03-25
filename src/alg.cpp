85fee86a4cfa66611a32e40d2195d01f22936061
katerinadum <katya05102002@gmail.com>
2021-03-25 21:54:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,7 @@
 int cbinsearch(int *arr, int size, int value) {
   int i = 0, j = size-1; //i - левая граница массива, j - правая
   int k = 0; //счетчик
-  while (i < j)
-  {
+  while (i < j) {
     int mid = i+(j-i)/2;
     if (arr[mid] == value)
      k++;
