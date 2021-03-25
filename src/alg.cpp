ca8d3a291e709413c33a2f8016393c0d434832de
katerinadum <katya05102002@gmail.com>
2021-03-25 20:38:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int i=0, j=size-1; //i - левая граница массива, j - правая
-  int k=0; //счетчик
-  while (i<j)
+  int i = 0, j = size-1; //i - левая граница массива, j - правая
+  int k = 0; //счетчик
+  while (i < j)
   {
-    int mid=i+(j-i)/2;
-    if (arr[mid]==value)
+    int mid = i+(j-i)/2;
+    if (arr[mid] == value)
      k++;
-    else if (arr[mid]>value)
-      j=mid;
+    else if (arr[mid] > value)
+      j = mid;
     else
-      i=mid+1;
+      i = mid+1;
   }
-  if (k>0)
+  if (k > 0)
     return k;
   return 0; // если ничего не найдено
 }
